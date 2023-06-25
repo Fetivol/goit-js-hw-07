7286a484d3175b5f481958806020fb51385d59a6
Fetivol <Fetisowova@gmail.com>
2023-06-25 09:26:31 +0200

    task 2
    SimpleLightbox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -7,7 +7,7 @@ const liElements = galleryItems
     ({ preview, original, description }) =>
       `
     <li class="gallery__item">
-  <a class="gallery__link" href="${original}" onclick="event.preventDefault()"
+  <a class="gallery__link" href="${original}"
   >
     <img
       class="gallery__image"
@@ -23,22 +23,36 @@ const liElements = galleryItems
 container.insertAdjacentHTML('beforeend', liElements);
 
 container.addEventListener('click', evt => {
+  evt.preventDefault();
   if (!evt.target.classList.contains('gallery__image')) {
     return;
   }
-  const instance = basicLightbox.create(`
+  const instance = basicLightbox.create(
+    `
     <img src="${evt.target.dataset.source}" width="800" height="600">
-    `);
+    `,
+    {
+      onShow: document.addEventListener('keydown', evt => {
+        if (evt.key === 'Escape') {
+          instance.close();
+        }
+      }),
+      onClose: document.removeEventListener('keydown', evt => {
+        if (evt.key === 'Escape') {
+          instance.close();
+        }
+      }),
+    }
+  );
   instance.show();
 
-  closingModal(instance);
+  //   closingModal(instance);
 });
 
-function closingModal(modal) {
-  document.addEventListener('keydown', evt => {
-    evt.preventDefault();
-    if (evt.key === 'Escape' && modal.visible()) {
-      modal.close();
-    }
-  });
-}
+// function closingModal(modal) {
+//     document.addEventListener('keydown', evt => {
+//         if (evt.key === 'Escape' && modal.visible()) {
+//             modal.close();
+//         }
+//     });
+// }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,43 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const container = document.querySelector('.gallery');
+
+const liElements = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `
+    <li class="gallery__item">
+  <a class="gallery__link" href="${original}"
+  >
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>
+`
+  )
+  .join('');
+container.insertAdjacentHTML('beforeend', liElements);
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  navText: ['<', '>'],
+});
+
+container.addEventListener('click', evt => {
+  evt.preventDefault();
+  if (!evt.target.classList.contains('gallery__image')) {
+    return;
+  }
+
+  gallery.on('show.simplelightbox', () => {
+    console.log(
+      gallery,
+      gallery.options.captionsData,
+      gallery.options.captionDelay
+    );
+  });
+});
